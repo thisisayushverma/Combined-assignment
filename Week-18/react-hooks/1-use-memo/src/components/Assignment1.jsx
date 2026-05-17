518f3356715bf1cb4733311eed5cbcd724a3b93e
Ayush Verma <ayushverma7052515243@gmail.com>
2026-05-17 14:09:36 +0530

    complete use-memo assignment

--- Week-18/react-hooks/1-use-memo/src/components/Assignment1.jsx
+++ Week-18/react-hooks/1-use-memo/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useMemo } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
@@ -6,7 +6,24 @@ import { useState } from "react";
 export function Assignment1() {
     const [input, setInput] = useState(0);
     // Your solution starts here
-    const expensiveValue = 0; 
+    
+    const calcFact = (value)=>{
+        console.log("safadsf");
+        
+        let ans= 1;
+        for(let i = 1; i<=value;i++){
+            ans*=i;
+        }
+        return ans;
+    }
+    
+    const expensiveValue = useMemo(()=>{
+        return calcFact(input);
+    },[input]); 
+
+    // const expensiveValue = calcFact(input);
+
+   
     // Your solution ends here
 
     return (
--- Week-18/react-hooks/1-use-memo/src/components/Assignment3.jsx
+++ Week-18/react-hooks/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,19 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 100 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(()=>{
+        let temp = 0;
+        for(let i = 0; i<items.length;i++){
+            temp+=items[i].value || 0;
+        }
+
+        return temp;
+    },[items]);
     // Your code ends here
     return (
         <div className='container'>
